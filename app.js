d4ade731cbe7f7469b6ecef5950770163f10017d
Jérôme <jerome.ceccato@gmail.com>
2018-03-05 19:34:38 +0100

    ssl

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express   = require('express');
-const app       = express();
-const port      = 3000;
+const fs = require('fs');
+const http = require('http');
+
+const express = require('express');
+const app = express();
 
 app.use(express.static(`${__dirname}/client`));
 require('./server/routes.js')(app);
 
-app.listen(port);
-console.log(`Web server listening on port ${port}`);
+const httpServer = http.createServer(app);
+
+httpServer.listen(3000);
+console.log('HTTP server started');
+
+if (fs.existsSync('sslcert/server.key')) {
+    const https = require('https');
+    const privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+    const certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+    const credentials = {key: privateKey, cert: certificate};
+
+    const httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(3443);
+    console.log('HTTPS server started');
+}
+
