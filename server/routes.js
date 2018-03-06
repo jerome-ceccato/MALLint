97ef67f719f5d223c1dd3cd7e7abf42d0f4cd518
Jérôme <jerome.ceccato@gmail.com>
2018-03-06 16:27:37 +0100

    Added logs

--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const logfile = 'logs/logs.json';
+
 module.exports = app => {
 
 	app.get(`/`, (req, res) => {
@@ -7,5 +10,17 @@ module.exports = app => {
 	app.get(`/analyze/:entity/:user`, (req, res) => {
 		let api = new (require('./api'))();
 		api.run(res, req.params);
+
+		log(req.params.entity, req.params.user);
 	});
 };
+
+function log(entity, user) {
+    let data = {
+        date: new Date().toISOString(),
+        entity: entity,
+        user: user,
+    };
+
+    fs.appendFile(logfile, JSON.stringify(data) + '\n', function (err) {});
+}
